12861c8103ef2f392cbeba37e32d9b3fad13ada7
zhangbaolong <18345535025@163.com>
2021-06-04 15:43:39 +0800

    perf

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  
 </template>
 
 <script lang="ts">
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>hello ~~</h1>
+  <h1>哈哈哈哈哈</h1>
 </template>
 
 <script lang="ts">
