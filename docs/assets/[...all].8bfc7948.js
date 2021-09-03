b6e275ec556039cc416484325bd123e2c8fc0f4f
zhangbaolong <18345535025@163.com>
2021-09-03 14:48:45 +0800

    fix

--- docs/assets/[...all].8bfc7948.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as r,a}from"./vendor.716abeea.js";const n={},o={class:"no-page text-center"},t=[a("h1",null,"Error 404",-1)];n.render=function(a,n){return e(),r("div",o,t)};export{n as default};
