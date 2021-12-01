c1a338c7112772b549207f3d6e652a36ecd71dd4
zhangbaolong <18345535025@163.com>
2021-12-01 08:26:22 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,19 @@
     <NavBar />
 
     <main class="px-7 py-10">
-      <router-view />
+      <router-view v-if="show" />
       <Footer></Footer>
     </main>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const show = ref(false)
+setTimeout(() => {
+  show.value = true
+}, 0)
+</script>
 
 <style></style>
